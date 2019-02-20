74e0f0d28128e5d936dccda83b16222b7d01f756
Daniel <daniel.olsson1992@gmail.com>
2019-02-20 13:00:13 +0100

    Translate data strings to int

--- Js/DataHandling.js
+++ Js/DataHandling.js
@@ -1,13 +1,20 @@
-function ReadData()
-{
-
-}
-
-
-
 
 function ParseData(data)
 {
-	console.log("Hej");
+	data.forEach(function(d)
+	{
+		d["Area Code"] = +d["Area Code"];
+		d["Item Code"] = +d["Item Code"];
+		d["Element Code"] = +d["Element Code"];
+		d["latitude"] = +d["latitude"];
+		d["longitude"] = +d["longitude"];
+		for(var i =1961; i<=2013;i++)
+		{
+			d["Y"+i] = +d["Y"+i]; 
+		}
+	})
+	
+	console.log(data);
+
 	return data;
 }
