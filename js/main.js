d9e8daeb3a516809b4fa48ddc465d21ed8295bdb
Bapiro <lucas.rung@gmail.com>
2019-02-26 10:52:52 +0100

    map

--- js/main.js
+++ js/main.js
@@ -4,8 +4,10 @@ var dataObject;
 d3.csv("/data/FAO.csv",function(data) {
   	//Parse the data into something useful.
   	dataObject =ParseData(data);
-    console.log("jag vill dö");
     FocusPlotContext(dataObject);
-});
 
-worldMap();
+    $.getJSON("/data/customLow.geo.json",function(wData){
+      worldMap(dataObject,wData);
+    })
+
+});
