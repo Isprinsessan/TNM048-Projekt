f5d9ca8ee38dc3a1dbba9fb2790742927e9caea3
Erik Nilsson <eriknilsssonn@gmail.com>
2019-03-05 11:24:18 +0100

    Created the different information divs

--- js/TooltipFunction.js
+++ js/TooltipFunction.js
@@ -2,17 +2,47 @@ function Information()
 {
 
   //Function to display the tooltip information
-  this.tooltip = function(d) {
+  this.tooltipCluster = function(d) {
 
     //Access the tooltip div
-    var tooltip = d3.select("#tooltip")
+    var tooltip = d3.select("#tooltip-cluster")
 
     //Select the country id in tooptip and change the text to the current country
-    tooltip.select("#country")
+    tooltip.select("#country-cluster")
         .text("Country: " + d["Area"]);
 
-    //Select the food id in tooptip and change the text 
-    tooltip.select("#food")
+    //Select the food id in tooptip and change the text
+    tooltip.select("#food-cluster")
+        .text("Produced food type: " + d["Item"]);
+
+  }
+
+  this.tooltipPlot = function(d) {
+
+    //Access the tooltip div
+    var tooltip = d3.select("#tooltip-plot")
+
+    //Select the country id in tooptip and change the text to the current country
+    tooltip.select("#country-plot")
+        .text("Country: " + d["Area"]);
+
+    //Select the food id in tooptip and change the text
+    tooltip.select("#food-plot")
+        .text("Produced food type: " + d["Item"]);
+
+  }
+
+  this.tooltipMap = function(d) {
+
+    //Access the tooltip div
+    var tooltip = d3.select("#tooltip-map")
+
+    //Select the country id in tooptip and change the text to the current country
+    tooltip.select("#country-map")
+        .text("Country: " + d["Area"]);
+
+    //Select the food id in tooptip and change the text
+    tooltip.select("#food-map")
         .text("Produced food type: " + d["Item"]);
 
   }
